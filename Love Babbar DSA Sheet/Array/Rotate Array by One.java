74edcf9e04a26e1fa64bb8009454f2d37217fe88
Vaishnavi Nagane <vaishnavinagane986@gmail.com>
2024-12-02 19:00:26 +0530

    Update Rotate Array by One.java

--- Love Babbar DSA Sheet/Array/Rotate Array by One.java
+++ Love Babbar DSA Sheet/Array/Rotate Array by One.java
@@ -9,3 +9,7 @@ class Solution {
         arr[0]=last;
     }
 }
+
+//Input: arr = [1, 2, 3, 4, 5]
+//Output: [5, 1, 2, 3, 4]
+//Explanation: If we rotate arr by one position in clockwise 5 come to the front and remaining those are shifted to the end.

c32833e66a117cefc8aa47c14d003600bfdb3779
Vaishnavi Nagane <vaishnavinagane986@gmail.com>
2024-12-02 19:49:52 +0530

    Update Minimize the Heights II.java

--- Love Babbar DSA Sheet/Array/Minimize the Heights II.java
+++ Love Babbar DSA Sheet/Array/Minimize the Heights II.java
@@ -1,3 +1,8 @@
+/*Input: k = 2, arr[] = {1, 5, 8, 10}
+Output: 5
+Explanation: The array can be modified as {1+k, 5-k, 8-k, 10-k} = {3, 3, 6, 8}.The difference between the largest and 
+the smallest is 8-3 = 5.*/
+
 class Solution {
     int getMinDiff(int[] arr, int k) {
         // code here
